3e496e43f97d18b722a8286af4b83a3246dbeb7f
Brandon Brinkley <14953111+brandonbrinkley@users.noreply.github.com>
2023-08-23 13:29:41 -0500

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as ET
 
+filein  = 'borrowed.xml'
+fileout = 'transformed.xml'
 #namespaces = {
 #    'dc': 'http://purl.org/dc/elements/1.1/',
 #    'content': 'http://purl.org/rss/1.0/modules/content/',
@@ -7,13 +9,13 @@ import xml.etree.ElementTree as ET
 #    'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
 #    'anchor': 'https://anchor.fm/xmlns'
 #}
-namespaces = dict([node for _, node in ET.iterparse(filename, events=['start-ns'])])
+namespaces = dict([node for _, node in ET.iterparse(filein, events=['start-ns'])])
 for ns in namespaces:
     ET.register_namespace(ns, namespaces[ns])
         
-tree = ET.parse('borrowed.xml')
+tree = ET.parse(filein)
 
 for type in tree.getroot().iterfind('itunes:type', namespaces):
     type.text = "serial"
 
-tree.write('transformed.xml')
+tree.write(fileout)
