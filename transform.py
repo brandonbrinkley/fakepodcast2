5c20b1050fa15e44efeee920d3097b35099ee50d
Brandon Brinkley <14953111+brandonbrinkley@users.noreply.github.com>
2023-08-23 12:17:08 -0500

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,1 +1,8 @@
+import xml.etree.ElementTree as ET
 
+tree = ET.parse('borrowed.xml')
+
+for type in tree.getroot().iterfind('itunes:type'):
+    type.text = "serial"
+
+tree.write('transformed.xml')
