ecae5640676c93cb3f1499c5dfef815ec617efd4
Brandon Brinkley <14953111+brandonbrinkley@users.noreply.github.com>
2023-08-23 13:00:06 -0500

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,6 +1,12 @@
 import xml.etree.ElementTree as ET
 
-namespaces = {'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd'}
+namespaces = {
+    'dc': 'http://purl.org/dc/elements/1.1/',
+    'content': 'http://purl.org/rss/1.0/modules/content/',
+    'atom': 'http://www.w3.org/2005/Atom',
+    'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+    'anchor': 'https://anchor.fm/xmlns'
+}
 
 tree = ET.parse('borrowed.xml')
 
