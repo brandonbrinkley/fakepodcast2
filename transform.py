b35d83eb1804f93e603d6fa64c1fb3e7c57ba354
Brandon Brinkley <14953111+brandonbrinkley@users.noreply.github.com>
2023-08-23 12:41:45 -0500

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,8 +1,10 @@
 import xml.etree.ElementTree as ET
 
+namespaces = {'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd'}
+
 tree = ET.parse('borrowed.xml')
 
-for type in tree.getroot().iterfind('itunes:type'):
+for type in tree.getroot().iterfind('itunes:type', namespaces):
     type.text = "serial"
 
 tree.write('transformed.xml')
