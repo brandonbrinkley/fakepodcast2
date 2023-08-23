73361bf7f44344cab44dfbcb68b5611eb4165419
Brandon Brinkley <14953111+brandonbrinkley@users.noreply.github.com>
2023-08-23 13:47:49 -0500

    Update transform.py

--- transform.py
+++ transform.py
@@ -2,20 +2,18 @@ import xml.etree.ElementTree as ET
 
 filein  = 'borrowed.xml'
 fileout = 'transformed.xml'
-#namespaces = {
-#    'dc': 'http://purl.org/dc/elements/1.1/',
-#    'content': 'http://purl.org/rss/1.0/modules/content/',
-#    'atom': 'http://www.w3.org/2005/Atom',
-#    'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
-#    'anchor': 'https://anchor.fm/xmlns'
-#}
+
+# Register the namespaces in the original feed
 namespaces = dict([node for _, node in ET.iterparse(filein, events=['start-ns'])])
 for ns in namespaces:
     ET.register_namespace(ns, namespaces[ns])
-        
+
+# Parse the feed file
 tree = ET.parse(filein)
 
-for type in tree.getroot().iterfind('itunes:type', namespaces):
+# Change the channel type from episodic to serial
+for type in tree.getroot().iter('itunes:type'):
     type.text = "serial"
 
+# Output the new feed file
 tree.write(fileout)
